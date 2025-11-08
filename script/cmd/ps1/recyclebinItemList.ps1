90db00be8eb260e9818e2b393bb155501d3461b4
tositadanakada <tositadanakada@gmail.com>
2025-11-08 13:30:01 +0900

    fix: add maxresults to ps1 . Search result are limited because powershell break  due to overflows.

--- script/cmd/ps1/recyclebinItemList.ps1
+++ script/cmd/ps1/recyclebinItemList.ps1
@@ -1,14 +1,16 @@
 $recycleBin = (New-Object -ComObject Shell.Application).NameSpace(0x0a) # open fileobject $Recycle.Bin
-$recycleBinItems = $recycleBin.Items()
-
+$maxfileCount = 30
+$recycleBinItems = $recycleBin.Items() | Select-Object -First $maxfileCount
+$maxfileCount -= $recycleBinItems.Count
 # C:\\$Recycle.Bin\\S-1-5-21-userSid\\failehash.extend  ex) C:\\$Recycle.Bin\\S-1-5-21-xxxx\\$RVOP46T\\aaadir\\aaa.txt
 
-if ($recycleBinItems.Count  -eq 0){
+if ($recycleBinItems.Count -eq 0){
   $noneobj=@{
     type = "notfound"
   }
   convertTo-Json $noneobj # return $Recycle.Bin none
 }else{
+
   foreach ($recycleBinItem in $recycleBinItems)
   {
     if (!$recycleBinItem.IsFolder){
@@ -30,33 +32,38 @@ if ($recycleBinItems.Count  -eq 0){
       #echo $recycleBinItem | Get-Member
     }
   
-  if ($recycleBinItem.IsFolder){
+    if ($recycleBinItem.IsFolder){
       $filepath = $recycleBinItem.Path
       $rootfolderName = $recycleBinItem.Name
       $rootfolderrestorepath = $recycleBinItem.ExtendedProperty("System.Recycle.DeletedFrom")
       $splitrootpath = $filepath.split("\\",4)
       $roothash = $splitrootpath[$splitrootpath.length-1]
-      $chiledlen = Get-ChildItem -Path $filepath -File -Force -Recurse
-      $abstPath = $chiledlen.FullName
-      $splitabstPath = $chiledlen.FullName.split("\\",4)
-      $relativePath = $splitabstPath[$splitabstPath.length-1]
-      $virtualPath = $relativePath.Replace($roothash,$rootfolderName)
-      $filename = $chiledlen.Name
+      $chiledlen = Get-ChildItem -Path $filepath -File -Force -Recurse  | Select-Object -First $maxfileCount
+      $maxfileCount -= $chiledlen.Count
+      foreach ($childItem in $chiledlen){
+        $abstPath = $childItem.FullName
+        $splitabstPath = $childItem.FullName.split("\\",4)
+        $relativePath = $splitabstPath[$splitabstPath.length-1]
+        $virtualPath = $relativePath.Replace($roothash,$rootfolderName)
+        $filename = $childItem.Name
       
-
-      $childobj=@{
-        type = "childobj"
-        rootfolderrestorepath= $rootfolderrestorepath;
-        rootfolderName= $rootfolderName;
-        filename=$filename;
-        abstPath=$abstPath;
-        relativePath = $relativePath
-        virtualPath = $virtualPath ;
-        restorePath = $rootfolderrestorepath;
-        roothash = $roothash
+        $childobj=@{
+          type = "childobj"
+          rootfolderrestorepath= $rootfolderrestorepath;
+          rootfolderName= $rootfolderName;
+          filename=$filename;
+          abstPath=$abstPath;
+          relativePath = $relativePath
+          virtualPath = $virtualPath ;
+          restorePath = $rootfolderrestorepath;
+          roothash = $roothash
+        }
+        convertTo-Json $childobj -Compress
       }
       #echo $chiledlen | Get-Member
-      convertTo-Json $childobj -Compress
-    }  
+    }
+    if ($maxfileCount -le 0){
+      return
+    }
   }
 }
